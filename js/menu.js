2af94f6ee848205ad7d9aec468626280998011a9
alexander-carrels <alexander.carrels@outlook.com>
2026-05-15 14:26:48 +0200

    try to fix menu bug on safari

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   if (!placeholder) return;
 
   const pathPrefix = window.location.pathname === "/" ? "" : "../";
+
   const res = await fetch(`${pathPrefix}portfolio-menu.html`, {
     cache: "no-cache",
   });
@@ -10,7 +11,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   placeholder.innerHTML = await res.text();
 
   const dropdown = placeholder.closest(".drop-down");
-  const toggle = dropdown.querySelector(".portfolio-toggle, > a");
+  const toggle = dropdown.querySelector(".portfolio-toggle");
 
   if (!toggle) return;
 
@@ -18,13 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     e.preventDefault();
     e.stopPropagation();
 
-    dropdown.classList.toggle("is-open");
-
-    if (toggle.hasAttribute("aria-expanded")) {
-      toggle.setAttribute(
-        "aria-expanded",
-        dropdown.classList.contains("is-open") ? "true" : "false"
-      );
-    }
+    const isOpen = dropdown.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
   });
 });
